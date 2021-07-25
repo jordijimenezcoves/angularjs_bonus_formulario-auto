248e88e6e290f4c49ff2b08f7a45b7e539d2b7ba
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2021-07-25 11:51:37 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+var app = angular.module('bonusApp',[ 'jcs-autoValidate' ]);
+
+angular.module('bonusApp')
+    .run([
+    'defaultErrorMessageResolver',
+    function (defaultErrorMessageResolver) {
+        // passing a culture into getErrorMessages('fr-fr') will get the culture specific messages
+        // otherwise the current default culture is returned.
+        defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+          errorMessages['edadMinima'] = 'La edad mínima es de {0} años';
+          errorMessages['edadMaxima'] = 'La edad máxima es de {0} años';
+        });
+    }
+]);
+
+app.controller('mainCtrl', ['$scope', function($scope){
+
+	$scope.formData = {};
+
+
+	$scope.guardar_datos = function( valido ){
+
+		if( !valido ){
+			return;
+		}
+
+
+
+		console.log("Posteando...");
+
+
+	}
+
+
+	
+}]);
